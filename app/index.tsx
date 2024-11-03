0c9d8152d84ce67b4f8f80cfbe6d92de7eac3568
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-11-03 10:38:18 +0200

    Create Landing Page

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,27 @@
-import { Text, View } from "react-native";
+import PrimaryButton from "@/components/buttons/PrimaryButton";
+import SecondaryButton from "@/components/buttons/SecondaryButton";
+import { Link } from "expo-router";
+import { Modal, Text, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+	return (
+		<>
+			
+			<View className="flex flex-col gap-6 p-8 bg-[#F6F6F6]">
+				
+				<Text className="text-4xl font-semibold">Welcome to Student Loans eBonding</Text>
+				<View className="justify-center items-center w-full h-80 bg-[#C9C9C9]"><Text>Image Placeholder</Text></View>
+				<Text className="text-xl font-semibold">This platform enables the loan bonding process to be completed digitally. Get started with an option below:</Text>
+				
+				<View className="flex flex-col gap-8 items-center">
+					<Link href="/sign-up">
+						<PrimaryButton text="Create Account" />
+					</Link>
+					<Link href="/sign-in">
+						<SecondaryButton text="Already have an Account?" />
+					</Link>
+				</View>
+			</View>
+		</>
+	);
 }
