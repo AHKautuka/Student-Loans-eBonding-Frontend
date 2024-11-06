ebc00b981edd0ae56a6cabff822c86c5bf626bb8
Caroline-Nkhoma <167551771+Caroline-Nkhoma@users.noreply.github.com>
2024-11-06 21:30:08 +0200

    added text input field

--- app/screen/loan-amount.tsx
+++ app/screen/loan-amount.tsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import { Text,View } from 'react-native'
-export default function LoanAmount(){
-    return (
-        <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      > <Text>loanAmount</Text></View>
-    )
+import React from 'react';
+import { Image, Text, View } from 'react-native';
+import InputField from '../components/input-field';
+
+export default function LoanAmount() {
+  return (
+    <View
+      style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+      }}
+    >
+      <Text style={{ fontSize: 35, fontWeight: 'bold', marginTop:-400, marginLeft: -100 }}>
+        Loan Amount
+      </Text>
+      <Image
+        source={require('../../assets/images/loans-board-logo.jpeg')}
+        style={{ width: 250, height: 250, resizeMode: 'contain', marginBottom: 20 }}
+      />
+      <Text style={{ fontSize: 20, fontWeight: 'regular', marginBottom: 20 }}>
+        Approved Loan Amount
+      </Text>
+      <InputField />
+    </View>
+  );
 }
