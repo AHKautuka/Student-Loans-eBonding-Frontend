d64a9792b0d525deb90c6832c262a17d52ce6bba
Caroline-Nkhoma <167551771+Caroline-Nkhoma@users.noreply.github.com>
2024-11-06 20:28:36 +0200

    added loan amount screen and button in components

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
-import { Text, View } from "react-native";
-
+import { View } from "react-native";
+import LoanAmount from "./screen/loan-amount";
 export default function Index() {
   return (
     <View
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+   <LoanAmount/>
     </View>
   );
 }
