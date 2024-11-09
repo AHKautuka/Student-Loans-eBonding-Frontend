f5ac1fac495205de3a06462fb95187d4c68b2658
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-11-09 11:56:52 +0200

    Change style

--- components/TextField.tsx
+++ components/TextField.tsx
@@ -11,6 +11,6 @@ interface TextFieldProps {
 
 export default function TextField(props: TextFieldProps) {
 	return (
-		<TextInput placeholder={props.placeholder} onBlur={props.onBlur} onChangeText={props.onChangeText} value={props.value} editable={props.editable} secureTextEntry={props.secureTextEntry} style={{ backgroundColor: "#D9D9D9", color: "#454545", paddingVertical: 12, paddingHorizontal: 30, fontFamily: "Inter" }} className="text-xl font-semibold" />
+		<TextInput placeholder={props.placeholder} onBlur={props.onBlur} onChangeText={props.onChangeText} value={props.value} editable={props.editable} secureTextEntry={props.secureTextEntry} style={{ backgroundColor: "#D9D9D9", color: "#454545", paddingVertical: 12, paddingHorizontal: 30, fontFamily: "Inter" }} className="text-lg font-semibold" />
 	);
 }
