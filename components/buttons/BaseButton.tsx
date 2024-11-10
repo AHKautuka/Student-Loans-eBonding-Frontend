4452614d842a732bab906675c3421d1583138fd3
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-11-10 06:59:26 +0200

    Refactor Text
    - Use Button Text instead of default React-Native Text

--- components/buttons/BaseButton.tsx
+++ components/buttons/BaseButton.tsx
@@ -1,4 +1,5 @@
 import { ColorValue, GestureResponderEvent, Pressable, Text, View } from "react-native";
+import ButtonText from "../text/ButtonText";
 
 export interface BaseButtonProps {
 	text: string;
@@ -17,7 +18,9 @@ export default function BaseButton(props: BaseButtonProps) {
 	return (
 		<Pressable onPress={props.onPress} disabled={props.disabled}>
 			<View style={{ alignSelf: 'center', backgroundColor: props.bgColor, paddingVertical: 12, paddingHorizontal: 30, borderRadius: 10, borderWidth: props.border, borderColor: props.borderColor }} className={`justify-center items-center ${props.className}`}>
-				<Text style={{ fontFamily: "Inter", color: props.textColor }} className={`text-xl font-semibold`}>{props.text}</Text>
+				<ButtonText color={props.textColor!}>
+					{props.text}
+				</ButtonText>
 			</View>
 		</Pressable>
 	);
