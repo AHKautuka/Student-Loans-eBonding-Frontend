e7c9afaae9872d8b59976aed95f6b80020c60eac
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-11-11 15:27:53 +0200

    Change style for Header and TabBar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,12 +3,12 @@ import { Tabs } from "expo-router";
 
 export default function TabLayout() {
 	return (
-		<Tabs screenOptions={{ tabBarActiveTintColor: "#997B2F" }}>
+		<Tabs screenOptions={{ headerStyle: { height: 76 }, headerTitle: "", tabBarStyle: { height: 76 }, tabBarLabelPosition: "below-icon", tabBarLabelStyle: { fontFamily: "Inter", fontSize: 12 }, tabBarActiveTintColor: "#997B2F" }}>
 			<Tabs.Screen
 				name="home"
 				options={{
 					title: 'Home',
-					tabBarIcon: ({ color }) => <FontAwesome6 size={36} name="house" color={color} />,
+					tabBarIcon: ({ color }) => <FontAwesome6 size={24} name="house" color={color} />,
 				}}
 			/>
 		</Tabs>
