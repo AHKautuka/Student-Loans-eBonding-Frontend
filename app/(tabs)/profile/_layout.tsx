da77236c40eacbe345c88c60d21bd2be3928ade9
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-11-13 11:19:14 +0200

    Fix navigation
    - Use Stack navigation within Profile tab

---
+++ app/(tabs)/profile/_layout.tsx
@@ -0,0 +1,13 @@
+import Logo from "@/components/Logo";
+import { FontAwesome6 } from "@expo/vector-icons";
+import { Stack, Tabs } from "expo-router";
+
+export default function TabLayout() {
+	return (
+		<Stack screenOptions={{
+			headerShown: false,
+		}}>
+			
+		</Stack>
+	);
+}
