871ab65d1d8941226def3fed5371a2f544e6a1ef
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-11-13 08:52:16 +0200

    Refactor Details Card

--- components/cards/DetailsCard.tsx
+++ components/cards/DetailsCard.tsx
@@ -1,13 +1,13 @@
-import { View } from "react-native";
+import { PropsWithChildren } from "react";
+import { StyleProp, View, ViewStyle } from "react-native";
 
-interface DetailsCardProps {
-	className?: string;
-	children: React.ReactNode;
+interface DetailsCardProps extends PropsWithChildren {
+	style?: StyleProp<ViewStyle>;
 }
 
 export default function DetailsCard(props : DetailsCardProps) {
 	return (
-		<View style={{ alignSelf: "center", padding: 20, borderWidth: 1, borderRadius: 30, gap: 20 }} className="w-[21.75rem] h-fit bg-white border-[#959595]">
+		<View style={{ alignSelf: "center", width: 348, padding: 20, marginBottom: 32, borderWidth: 1, borderRadius: 30, ...(props.style as object) }} className="h-fit bg-white border-[#959595]">
 			{props.children}
 		</View>
 	);
