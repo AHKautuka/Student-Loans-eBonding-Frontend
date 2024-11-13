75e63b71d46603df33586391527abb445f1d0a92
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-11-13 08:53:00 +0200

    Create Personal Details Card

---
+++ components/cards/PersonalDetailsCard.tsx
@@ -0,0 +1,56 @@
+import DetailsCard from "./DetailsCard";
+import AppText, { fontWeight } from "../text/AppText";
+import DetailNameText from "../text/DetailNameText";
+import { Text } from "react-native";
+import DetailValueText from "../text/DetailValueText";
+import SecondaryButton from "../buttons/SecondaryButton";
+
+interface PersonalDetailsCardProps {
+	
+}
+
+export default function PersonalDetailsCard(props : PersonalDetailsCardProps) {
+	return (
+		<DetailsCard style={{ gap: 6 }}>
+			<AppText style={{ paddingBottom: 12 }} fontSize={20} lineHeight={24} fontWeight={fontWeight.semibold}>Personal Details</AppText>
+			
+			<Text>
+				<DetailNameText>Full Name:</DetailNameText>
+				<DetailValueText></DetailValueText>
+			</Text>
+			<Text>
+				<DetailNameText>Date of Birth:</DetailNameText>
+				<DetailValueText></DetailValueText>
+			</Text>
+			<Text>
+				<DetailNameText>Sex:</DetailNameText>
+				<DetailValueText></DetailValueText>
+			</Text>
+			<Text>
+				<DetailNameText>Postal Address:</DetailNameText>
+				<DetailValueText></DetailValueText>
+			</Text>
+			<Text>
+				<DetailNameText>Home Village:</DetailNameText>
+				<DetailValueText></DetailValueText>
+			</Text>
+			<Text>
+				<DetailNameText>Traditional Authority:</DetailNameText>
+				<DetailValueText></DetailValueText>
+			</Text>
+			<Text>
+				<DetailNameText>District:</DetailNameText>
+				<DetailValueText></DetailValueText>
+			</Text>
+			<Text>
+				<DetailNameText>Phone number:</DetailNameText>
+				<DetailValueText></DetailValueText>
+			</Text>
+			<Text>
+				<DetailNameText>National ID Number:</DetailNameText>
+				<DetailValueText></DetailValueText>
+			</Text>
+			<SecondaryButton text="Edit" />
+		</DetailsCard>
+	);
+}
