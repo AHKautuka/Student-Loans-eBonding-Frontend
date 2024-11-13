5b43096b0feca94cda09dc5bcc44bfec3d23258b
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-11-13 08:50:26 +0200

    Add students endpoint

--- utils/endpoints.ts
+++ utils/endpoints.ts
@@ -2,3 +2,4 @@ const baseURL = process.env.EXPO_PUBLIC_API_URL;
 
 export const urlAccounts = `${baseURL}/accounts`;
 export const urlUsers = `${baseURL}/users`;
+export const urlStudents = `${baseURL}/students`;
