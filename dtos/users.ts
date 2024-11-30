638c5825f4229b31271242d7d880bfce408d6e65
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-11-30 10:00:00 +0200

    Add userModel
    - Exports it aswell
    - Define and export a defaultUserModel
    - Define and export a dto to model converter function

--- dtos/users.ts
+++ dtos/users.ts
@@ -1,6 +1,41 @@
 export interface userReadDTO {
-	id: number,
-	accountId: string,
-	signature?: string 
-	profilePicture?: string
+	id: number;
+	accountId: string;
+	firstName?: string;
+	surname?: string;
+	otherNames?: string[];
+	signature?: string;
+	profilePicture?: string;
+}
+
+export interface userModel {
+	id: number;
+	accountId: string;
+	firstName: string;
+	surname: string;
+	otherNames: string[];
+	signature?: string;
+	profilePicture?: string;
+}
+
+export const defaultUserModel: userModel = {
+	id: 0,
+	accountId: "",
+	firstName: "",
+	surname: "",
+	otherNames: [],
+	signature: undefined,
+	profilePicture: undefined,
+}
+
+export function convertUserDTOtoModel(dto: userReadDTO): userModel {
+	return {
+		id: dto.id,
+		accountId: dto.accountId,
+		firstName: dto.firstName ?? "",
+		surname: dto.surname ?? "",
+		otherNames: dto.otherNames ?? [],
+		signature: dto.signature,
+		profilePicture: dto.profilePicture,
+	}
 }
