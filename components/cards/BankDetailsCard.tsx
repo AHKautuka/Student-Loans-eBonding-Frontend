2d083e3c6d4ddc2c1e27e15567e875e042833002
AHKautuka <bsc-com-01-22@unima.ac.mw>
2024-11-30 10:17:40 +0200

    Update BankDetailsCard
    - Accept a studentModel
    - Replace DetailNameText and DetailValueText components in Text combo with DetailText component

--- components/cards/BankDetailsCard.tsx
+++ components/cards/BankDetailsCard.tsx
@@ -1,13 +1,12 @@
 import DetailsCard from "./DetailsCard";
 import AppText, { fontWeight } from "../text/AppText";
-import DetailNameText from "../text/DetailNameText";
-import { Text } from "react-native";
-import DetailValueText from "../text/DetailValueText";
 import SecondaryButton from "../buttons/SecondaryButton";
 import { router } from "expo-router";
+import { studentModel } from "@/dtos/students";
+import DetailText from "../text/DetailText";
 
 interface BankDetailsCardProps {
-	
+	studentModel: studentModel,
 }
 
 export default function BankDetailsCard(props : BankDetailsCardProps) {
@@ -15,22 +14,11 @@ export default function BankDetailsCard(props : BankDetailsCardProps) {
 		<DetailsCard style={{ gap: 6 }}>
 			<AppText style={{ paddingBottom: 12 }} fontSize={20} lineHeight={24} fontWeight={fontWeight.semibold}>Bank Details</AppText>
 			
-			<Text>
-				<DetailNameText>Bank Name:</DetailNameText>
-				<DetailValueText></DetailValueText>
-			</Text>
-			<Text>
-				<DetailNameText>Branch Name:</DetailNameText>
-				<DetailValueText></DetailValueText>
-			</Text>
-			<Text>
-				<DetailNameText>Account Name:</DetailNameText>
-				<DetailValueText></DetailValueText>
-			</Text>
-			<Text>
-				<DetailNameText>Account Number:</DetailNameText>
-				<DetailValueText></DetailValueText>
-			</Text>
+			<DetailText detailName="Bank Name:" detailValue={props.studentModel.bankName} />
+			<DetailText detailName="Branch Name:" detailValue={props.studentModel.branchName} />
+			<DetailText detailName="Account Name:" detailValue={props.studentModel.bankAccountName} />
+			<DetailText detailName="Account Number:" detailValue={props.studentModel.bankAccountNumber} />
+			
 			<SecondaryButton onPress={() => router.navigate("/profile/bank-details")} text="Edit" />
 		</DetailsCard>
 	);
