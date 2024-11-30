d728a391da13a51bf71cce706eb2bb04822aad67
bsc-com-10-20 <bsc-com-10-20@unima.ac.mw>
2024-11-30 20:02:41 +0100

    committed endpoint

--- utils/endpoints.ts
+++ utils/endpoints.ts
@@ -3,3 +3,4 @@ const baseURL = process.env.EXPO_PUBLIC_API_URL;
 export const urlAccounts = `${baseURL}/accounts`;
 export const urlUsers = `${baseURL}/users`;
 export const urlStudents = `${baseURL}/students`;
+export const urlBondingPeriods = `${baseURL}/bonding-periods`;
